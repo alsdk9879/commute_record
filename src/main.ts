2df02e55370066ec0297366aca80021020fce724
alsdk9879 <alsdk9879@gmail.com>
2025-08-07 12:14:22 +0900

    change account

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-import './assets/less/main.less';
+import "./assets/less/main.less";
 
-import { createApp, ref } from 'vue';
-import App from './App.vue';
-import router from './router';
-import { Skapi } from 'skapi-js';
+import { createApp, ref } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { Skapi } from "skapi-js";
 
 const app = createApp(App);
 
-// const skapi = new Skapi(
-//   'ap21XhrgjG0KlulkR98Z',
-//   'f498d188-1fa5-43e5-a32d-904d3e125983',
-//   { autoLogin: true },
-//   { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
-// );
+const skapi = new Skapi(
+    "ap21ssrro9Hqw4DDvzbj",
+    "47d0592d-ab98-4b1a-b43d-2e59c73c25e3",
+    { autoLogin: true },
+    { hostDomain: "skapi.app", target_cdn: "d1wrj5ymxrt2ir" }
+); // 개발환경 test5 계정 > service3
 
-const skapi = new Skapi('ap22Xhpv3l8UI7PJ0zmZ', '80b7a637-0258-4701-9cef-68ad6ed43b7f');
+// const skapi = new Skapi('ap22Xhpv3l8UI7PJ0zmZ', '80b7a637-0258-4701-9cef-68ad6ed43b7f');
 
 app.use(router);
-app.mount('#app');
+app.mount("#app");
 
 export { skapi };
