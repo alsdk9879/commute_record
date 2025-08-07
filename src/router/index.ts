f50d6a4529d27acfbde1f5e1d87ff8154c63cf81
alsdk9879 <alsdk9879@gmail.com>
2025-08-07 17:54:06 +0900

    add: 404 error page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,55 +1,66 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '@/views/login.vue';
-import Signup from '@/views/signup.vue';
-import Confirm from '@/views/confirmation_required.vue';
-import CommuteSave from '@/views/commute_save.vue';
-import AdminCommute from '@/views/admin_commute.vue';
-import AdminCommuteDetail from '@/views/admin_commute_detail.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/login.vue";
+import Signup from "@/views/signup.vue";
+import Confirm from "@/views/confirmation_required.vue";
+import CommuteSave from "@/views/commute_save.vue";
+import AdminCommute from "@/views/admin_commute.vue";
+import AdminCommuteDetail from "@/views/admin_commute_detail.vue";
+import Error from "@/views/error.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: Login,
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: Signup,
-    },
-    {
-      path: '/confirmation-required',
-      name: 'confirmation-required',
-      component: Confirm,
-    },
-    {
-      path: '/commute-save',
-      name: 'commute-save',
-      component: CommuteSave,
-    },
-    {
-      path: '/commute-view-calendar',
-      name: 'commute-view-calendar',
-      component: () => import('@/views/commute_view_calendar.vue'),
-    },
-    {
-      path: '/admin',
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: '/admin-commute',
-          name: 'admin-commute',
-          component: AdminCommute,
+            path: "/",
+            name: "login",
+            component: Login,
         },
         {
-          path: '/admin-commute-detail/:userId',
-          name: 'admin-commute-detail',
-          component: AdminCommuteDetail,
+            path: "/signup",
+            name: "signup",
+            component: Signup,
         },
-      ],
-    },
-  ],
+        {
+            path: "/confirmation-required",
+            name: "confirmation-required",
+            component: Confirm,
+        },
+        {
+            path: "/commute-save",
+            name: "commute-save",
+            component: CommuteSave,
+        },
+        {
+            path: "/commute-view-calendar",
+            name: "commute-view-calendar",
+            component: () => import("@/views/commute_view_calendar.vue"),
+        },
+        {
+            path: "/admin",
+            children: [
+                {
+                    path: "/admin-commute",
+                    name: "admin-commute",
+                    component: AdminCommute,
+                },
+                {
+                    path: "/admin-commute-detail/:userId",
+                    name: "admin-commute-detail",
+                    component: AdminCommuteDetail,
+                },
+            ],
+        },
+        // 404 page
+        {
+            path: "/404page",
+            name: "404page",
+            component: Error,
+        },
+        {
+            path: "/:pathMatch(.*)*",
+            redirect: "/404page",
+        },
+    ],
 });
 
 export default router;
---
+++ src/views/error.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+#error
+    h2.title 404 페이지를 찾을 수 없습니다.
+</template>
+
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import { skapi } from "@/main";
+
+const router = useRouter();
+const route = useRoute();
+</script>
+
+<style scoped lang="less">
+.title {
+    text-align: center;
+    padding-bottom: 2rem;
+    border-bottom: 1px solid #ccc;
+}
+</style>
